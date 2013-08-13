51327093bf4a9f0a90d924f5f42ae569136dbec8
sgtcaze <andrescruz@outlook.com>
2013-08-12 22:35:38 -0700

    added tons of new features

--- src/main/java/com/sgtcaze/Leaper/Leaper.java
+++ src/main/java/com/sgtcaze/Leaper/Leaper.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -47,57 +50,94 @@ public class Leaper extends JavaPlugin implements Listener {
 		}
 
 	}
-
-	@EventHandler
-	public void onEntityDamage(EntityDamageEvent e) {
-		if (((e.getEntity() instanceof Player))
-				&& (e.getCause() == EntityDamageEvent.DamageCause.FALL)) {
-			Player p = (Player) e.getEntity();
-			if (p.hasPermission("leaper.use")) {
-				e.setCancelled(true);
-			}
-		}
-	}
-
+	
 	@EventHandler
 	public void onMove(PlayerMoveEvent event) {
-		if ((event.getPlayer().hasPermission("leaper.use"))
-				&& (event.getPlayer().getGameMode() != GameMode.CREATIVE)
-				&& (event.getPlayer().getLocation().getBlock()
-						.getRelative(BlockFace.DOWN).getType() != Material.AIR)) {
-			event.getPlayer().setAllowFlight(true);
+		Player player = event.getPlayer();
+		World world = player.getWorld();
+		if (this.config.getList("Worlds.EnabledWorlds").contains(
+				world.getName())) {
+			if ((event.getPlayer().getGameMode() != GameMode.CREATIVE)
+					&& (event.getPlayer().getLocation().getBlock()
+							.getRelative(BlockFace.DOWN).getType() != Material.AIR)) {
+				event.getPlayer().setAllowFlight(true);
+			}
+
+		}
+		if (this.config.getList("Worlds.DisabledWorlds").contains(
+				world.getName())) {
+			if ((event.getPlayer().getGameMode() != GameMode.CREATIVE)
+					&& (event.getPlayer().getLocation().getBlock()
+							.getRelative(BlockFace.DOWN).getType() != Material.AIR)) {
+				event.getPlayer().setAllowFlight(false);
+			}
+
 		}
 	}
 
 	@EventHandler
 	public void onFly(PlayerToggleFlightEvent event) {
 		Player player = event.getPlayer();
-		if ((player.hasPermission("leaper.use"))
-				&& (player.getGameMode() != GameMode.CREATIVE)) {
-			event.setCancelled(true);
-			player.setAllowFlight(false);
-			player.setFlying(false);
-			player.setVelocity(player.getLocation().getDirection()
-					.multiply(1.0D * this.multiply).setY(1.0 * this.height));
-			if (this.config.getString("BatTakeOff").equalsIgnoreCase("true")) {
-				player.getLocation()
-						.getWorld()
-						.playSound(player.getLocation(), Sound.BAT_TAKEOFF,
-								1.0F, -5.0F);
+		World world = player.getWorld();
+
+		if (this.config.getList("Worlds.EnabledWorlds").contains(
+				world.getName())) {
+			if ((player.getGameMode() != GameMode.CREATIVE)) {
+				event.setCancelled(true);
+				player.setAllowFlight(false);
+				player.setFlying(false);
+				player.setVelocity(player.getLocation().getDirection()
+						.multiply(1.0D * this.multiply).setY(1.0 * this.height));
+				player.setFallDistance(0);
+
 			}
-			if (this.config.getString("EnderDragonWings").equalsIgnoreCase(
-					"true")) {
-				player.getLocation()
-						.getWorld()
-						.playSound(player.getLocation(),
-								Sound.ENDERDRAGON_WINGS, 1.0F, -5.0F);
-			}
-			if (this.config.getString("ShootArrow").equalsIgnoreCase("true")) {
-				player.getLocation()
-						.getWorld()
-						.playSound(player.getLocation(), Sound.SHOOT_ARROW,
-								1.0F, -5.0F);
-			}
+		}
+
+		if (this.config.getString("Particles.Smoke").equalsIgnoreCase("true")) {
+			player.getLocation().getWorld()
+					.playEffect(player.getLocation(), Effect.SMOKE, 2000);
+		}
+
+		if (this.config.getString("Particles.MobSpawnerFlames")
+				.equalsIgnoreCase("true")) {
+			player.getLocation()
+					.getWorld()
+					.playEffect(player.getLocation(), Effect.MOBSPAWNER_FLAMES,
+							2004);
+		}
+
+		if (this.config.getString("Particles.EnderSignal").equalsIgnoreCase(
+				"true")) {
+			player.getLocation()
+					.getWorld()
+					.playEffect(player.getLocation(), Effect.ENDER_SIGNAL, 2003);
+		}
+
+		if (this.config.getString("Particles.PotionBreak").equalsIgnoreCase(
+				"true")) {
+			player.getLocation()
+					.getWorld()
+					.playEffect(player.getLocation(), Effect.POTION_BREAK, 2002);
+		}
+
+		if (this.config.getString("Sounds.BatTakeOff").equalsIgnoreCase("true")) {
+			player.getLocation()
+					.getWorld()
+					.playSound(player.getLocation(), Sound.BAT_TAKEOFF, 1.0F,
+							-5.0F);
+		}
+		if (this.config.getString("Sounds.EnderDragonWings").equalsIgnoreCase(
+				"true")) {
+			player.getLocation()
+					.getWorld()
+					.playSound(player.getLocation(), Sound.ENDERDRAGON_WINGS,
+							1.0F, -5.0F);
+		}
+		if (this.config.getString("Sounds.ShootArrow").equalsIgnoreCase("true")) {
+			player.getLocation()
+					.getWorld()
+					.playSound(player.getLocation(), Sound.SHOOT_ARROW, 1.0F,
+							-5.0F);
 		}
 	}
 }
